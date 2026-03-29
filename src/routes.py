ceb8c3aee3be024126f6d017c7cb700a533331be
Jorge Cereceda <jorgecerecedatrabajo@gmail.com>
2026-03-29 18:37:10 +0200

    separando swagger en archivos

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from flasgger import Swagger
+from flasgger import Swagger, swag_from
+
 
 # Importamos desde archivo de servicios (el que adaptamos antes)
 from usuarios import * 
@@ -10,87 +11,24 @@ app = Flask(__name__)
 cors = CORS(app)
 
 # Plantilla global de Swagger
-swagger_template = {
-    "swagger": "2.0",
-    "info": {
-        "title": "API de Usuarios",
-        "description": "API para manejar usuarios",
-        "version": "1.0.0"
-    },
-    "basePath": "/",
-    "schemes": ["http", "https"],
-    "definitions": {
-        "Usuario": {
-            "type": "object",
-            "required": ["nombre", "edad", "altura", "pais"],
-            "properties": {
-                "nombre": {"type": "string", "description": "Nombre completo del usuario"},
-                "edad": {"type": "integer", "description": "Edad del usuario"},
-                "altura": {"type": "number", "description": "Altura en metros"},
-                "pais": {"type": "string", "description": "País de residencia"}
-            },
-            "example": {
-                "nombre": "Luis",
-                "edad": 28,
-                "altura": 1.75,
-                "pais": "España"
-            }
-        }
-    }
-}
 
-swagger = Swagger(app, template=swagger_template)
-
+# Flasgger crea un documento Swagger global en memoria
+# @swag_from("...") Lee el archivo YAML y lo inyecta dentro del Swagger global
+Swagger(app, template_file="swagger/global_template.yml")
 
 # 1. Obtener todos los usuarios (Carlos, Ana, Luis...)
-# Flasgger usa comentarios tipo YAML dentro de cada ruta.
 @app.route("/usuarios", methods=['GET'])
+@swag_from("swagger/get_usuarios.yml")
 def get_usuarios():
     
-    """
-    Obtener todos los usuarios
-    ---
-    responses:
-      200:
-        description: Lista de usuarios
-        examples:
-          application/json:
-            [
-              {"id": 1, "altura": 1.75, "edad": 25, "nombre": "Carlos", "pais": "España"},
-              {"id": 2, "altura": 1.65, "edad": 30, "nombre": "Ana", "pais": "México"}
-            ]
-    """
     usuarios = get_all_usuarios()
     return jsonify(usuarios)
 
 # 2. Obtener un usuario específico por su ID
-# Flasgger usa comentarios tipo YAML dentro de cada ruta.
 @app.route("/usuarios/<user_id>", methods=['GET'])
+@swag_from("swagger/get_usuarios_id.yml")
 def get_usuario(user_id):
-    
-    """
-    Obtener usuario por ID
-    ---
-    parameters:
-      - name: user_id
-        in: path
-        type: string
-        required: true
-        description: ID del usuario
-    responses:
-      200:
-        description: Usuario encontrado
-        schema:
-          $ref: '#/definitions/Usuario'
-      404: 
-          description: Usuario no encontrado
-          schema: 
-            type: object 
-            properties: 
-              error: 
-                type: string 
-                example: "Usuario no encontrado"
-    """
+  
     user_id = int(user_id)
     
     usuario = get_usuario_by(user_id)
@@ -101,37 +39,10 @@ def get_usuario(user_id):
     return jsonify(usuario)
 
 # 3. Crear un nuevo usuario
-# Flasgger usa comentarios tipo YAML dentro de cada ruta.
 @app.route("/usuarios", methods=["POST"])
+@swag_from("swagger/post_usuarios.yml")
 def new_usuario():
 
-    """
-    Crear un nuevo usuario
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          $ref: '#/definitions/Usuario'
-    responses:
-      201:
-        description: Usuario creado correctamente
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-              example: "Usuario recibido"
-      400: 
-        description: Error en la solicitud (datos inválidos o vacíos) 
-        schema: 
-          type: object 
-          properties: 
-            error: 
-              type: string 
-              example: "No hay datos para actualizar"
-    """
     data = request.get_json()
     
     # 1. Body vacío
@@ -153,49 +64,10 @@ def new_usuario():
     return jsonify({"message": "Usuario recibido"}), 201
 
 # 4. Actualizar un usuario existente
-# Flasgger usa comentarios tipo YAML dentro de cada ruta.
 @app.route("/usuarios/<user_id>", methods=["PUT"])
+@swag_from("swagger/put_usuarios.yml")
 def update_user_route(user_id):
 
-    """
-    Actualizar un usuario
-    ---
-    parameters:
-      - name: user_id
-        in: path
-        type: string
-        required: true
-      - name: body
-        in: body
-        required: true
-        schema:
-          $ref: '#/definitions/Usuario'
-    responses:
-      200:
-        description: Usuario actualizado correctamente
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-              example: "Usuario actualizado"
-      400: 
-        description: Error en la solicitud (datos inválidos o vacíos) 
-        schema: 
-          type: object 
-          properties: 
-            error: 
-              type: string 
-              example: "No hay datos para actualizar"
-      404: 
-        description: Usuario no encontrado
-        schema: 
-          type: object 
-          properties: 
-            error: 
-              type: string 
-              example: "Usuario no encontrado"
-    """
     data = request.get_json()
 
     if not data:
@@ -219,30 +91,10 @@ def update_user_route(user_id):
     return jsonify({"message": "Usuario actualizado"}), 200
 
 # 5. Eliminar un usuario
-# Flasgger usa comentarios tipo YAML dentro de cada ruta.
 @app.route("/usuarios/<user_id>", methods=['DELETE'])
+@swag_from("swagger/delete_usuarios.yml")
 def delete_usuario_route(user_id):
 
-    """
-    Eliminar usuario
-    ---
-    parameters:
-      - name: user_id
-        in: path
-        type: string
-        required: true
-    responses:
-      200:
-        description: Usuario eliminado
-      404: 
-        description: Usuario no encontrado
-        schema: 
-          type: object 
-          properties: 
-            error: 
-              type: string 
-              example: "Usuario no encontrado"
-    """
     print('** Eliminando usuario ID:', user_id)
     eliminado = del_usuario(user_id)
 
@@ -262,63 +114,8 @@ def index():
     return render_template("panel.html", usuarios=usuarios)
 
 @app.route("/usuarios/<user_id>", methods=["PATCH"])
+@swag_from("swagger/patch_usuarios.yml")
 def patch_usuario_route(user_id):
-
-    """
-    Actualizar parcialmente un usuario
-    ---
-    parameters:
-      - name: user_id
-        in: path
-        type: string
-        required: true
-      - name: body
-        in: body
-        required: true
-        schema:
-          type: object
-          properties:
-            nombre:
-              type: string
-              description: Nombre completo del usuario
-            edad:
-              type: integer
-              description: Edad del usuario
-            altura:
-              type: number
-              description: Altura en metros
-            pais:
-              type: string
-              description: País de residencia
-          example:
-            nombre: "Ana"
-            altura: 1.68
-    responses:
-      200:
-        description: Usuario actualizado parcialmente
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-              example: "Usuario actualizado parcialmente"
-      400: 
-        description: Error en la solicitud (datos inválidos o vacíos) 
-        schema: 
-          type: object 
-          properties: 
-            error: 
-              type: string 
-              example: "No hay datos para actualizar"
-      404: 
-        description: Usuario no encontrado
-        schema: 
-          type: object 
-          properties: 
-            error: 
-              type: string 
-              example: "Usuario no encontrado"
-    """
 
     data = request.get_json()
     print('** PATCH usuario ID:', user_id, data)
